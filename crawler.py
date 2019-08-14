97306cb443f3fc5fed2de9d80d2cce29cbc59360
Sun0506 <asd8061812200@gmail.com>
2019-08-14 17:41:32 +0800

    add write file

--- crawler.py
+++ crawler.py
@@ -1,11 +1,19 @@
 import requests as req 
 import bs4  
 
-res = req.get('https://www.eyny.com/video')
+res = req.get('https://forum.gamer.com.tw/B.php?bsn=23805&subbsn=0')
 # print(res.text)
 soup = bs4.BeautifulSoup(res.text, 'lxml')
 # print(soup.text)
-titles = soup.find_all('a', {'target':'_blank'})
+titles = soup.find_all('a', {'class':'b-list__main__title'})
+
+names = []
 for title in titles:
-	if title != None:
-		print(title.string)
+		if title.string != '首篇已刪':
+			names.append(title.string)
+
+
+with open('eyny.txt', 'w') as f:
+	for name in names:
+		f.write(name + '\n')
+	print('我寫好了!')
