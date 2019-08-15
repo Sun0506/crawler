f6e6d19bec529dc701d7a9401c32ff34d8b7b515
Sun0506 <asd8061812200@gmail.com>
2019-08-15 16:35:31 +0800

    add titles & urls

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 import requests as req 
 import bs4  
 
+url = 'https://forum.gamer.com.tw/'
 res = req.get('https://forum.gamer.com.tw/B.php?bsn=23805&subbsn=0')
 # print(res.text)
 soup = bs4.BeautifulSoup(res.text, 'lxml')
@@ -8,12 +9,22 @@ soup = bs4.BeautifulSoup(res.text, 'lxml')
 titles = soup.find_all('a', {'class':'b-list__main__title'})
 
 names = []
+webs = []
 for title in titles:
-		if title.string != '首篇已刪':
-			names.append(title.string)
+	if title.string != '首篇已刪':
+		names.append(title.string)
+		webs.append(title.get('href'))
 
+urls = []
+for web in webs:
+	urls.append(url + web)
 
-with open('eyny.txt', 'w') as f:
-	for name in names:
-		f.write(name + '\n')
+baha = []
+bahas = dict(zip(names, urls))
+for key, value in bahas.items():
+	baha.append(key + ': ' + value)
+
+with open('baha.txt', 'w') as f:
+	for line in baha:
+		f.write(line + '\n')
 	print('我寫好了!')
